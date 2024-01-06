f9cd69ab4e879151d36240e3536e1386a6978837
josedhonatas <dhonatas@academico.ufs.br>
2024-01-06 10:54:59 -0300

    feat: settings

--- agendaCovid/settings.py
+++ agendaCovid/settings.py
@@ -118,23 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+# settings.py
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
 
-MEDIAFILES_DIRS = [
-    os.path.join(BASE_DIR, 'media')
-]
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'agendaCovid/static'),
+    os.path.join(BASE_DIR, 'agendaCovid', 'static'),
 ]
+
+
 
 LOGIN_URL = 'pag_inicial'
 
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 
 
 # Default primary key field type
