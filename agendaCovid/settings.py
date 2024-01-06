488adc8c27186469cc126fafdaee1cfe0617b6e7
josedhonatas <dhonatas@academico.ufs.br>
2024-01-06 14:04:57 -0300

    refactor: login

--- agendaCovid/settings.py
+++ agendaCovid/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/img/'
 
 MEDIAFILES_DIRS = [
-    os.path.join(BASE_DIR, 'media')
+    os.path.join(BASE_DIR, 'img')
 ]
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
