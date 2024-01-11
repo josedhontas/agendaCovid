3a345b3368c34a319ee2637e31fd0ed8f222c26e
josedhonatas <dhonatas@academico.ufs.br>
2024-01-10 22:08:10 -0300

    fix: allowed hosts

--- agendaCovid/settings.py
+++ agendaCovid/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k_x-%6vm99xabi8wy)&_^@o#v6^gaf#o+df=rdn*q3(+3dm60h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = 'agenda.CustomUser'
 
 
