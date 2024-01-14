3b721edd4460570cdf15088964cac124a5b02846
josedhonatas <dhonatas@academico.ufs.br>
2024-01-13 22:17:39 -0300

    fix: static files

--- agendaCovid/settings.py
+++ agendaCovid/settings.py
@@ -119,16 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/img/'
-
-MEDIAFILES_DIRS = [
-    os.path.join(BASE_DIR, 'img')
-]
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'agendaCovid/static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/img/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'img')
 
 
 LOGIN_URL = 'pag_inicial'
